2d6dc6dc86f92aba0cc77f6be40f4f4e9b53c927
Noah Gullo <noahgulloingram@gmail.com>
2026-06-19 17:55:50 -0700

    feat: add click count with a state variable

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -5,9 +5,11 @@ const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [click, setClick] = useState(0);
 
   const onButtonClick = (color) => () => {
     setBackgroundColor(color);
+    setClick(click + 1);
   };
 
   return (
@@ -27,6 +29,7 @@ function App() {
           {color}
         </button>
       ))}
+      <p className="count">{click}</p>
     </div>
   );
 }
